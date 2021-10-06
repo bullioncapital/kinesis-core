1c210ff2e546eee96c8e31f8ab277135ce6f6c99
D123605 <debarishi@leewayhertz.com>
2021-10-06 11:25:41 +0000

    InflationOpFrameChanges doApply Function modified

--- src/transactions/InflationOpFrame.cpp
+++ src/transactions/InflationOpFrame.cpp
@@ -10,6 +10,7 @@
 #include "main/Application.h"
 #include "overlay/StellarXDR.h"
 #include "transactions/TransactionUtils.h"
+#include "crypto/SHA.h"
 
 const uint32_t INFLATION_FREQUENCY = (60 * 60 * 24 * 7); // every 7 days
 // inflation is .000190721 per 7 days, or 1% a year
@@ -26,6 +27,109 @@ InflationOpFrame::InflationOpFrame(Operation const& op, OperationResult& res,
     : OperationFrame(op, res, parentTx)
 {
 }
+
+#ifdef _KINESIS
+
+// kinesis implementation
+bool
+InflationOpFrame::doApply(AbstractLedgerTxn& ltx)
+{
+    auto header = ltx.loadHeader();
+    auto& lh = header.current();
+    time_t closeTime = lh.scpValue.closeTime;
+    uint64_t seq = lh.inflationSeq;
+
+//    time_t inflationTime = (INFLATION_START_TIME + seq * INFLATION_FREQUENCY);
+//    if (closeTime < inflationTime)
+//    {
+//        innerResult().code(INFLATION_NOT_TIME);
+//        return false;
+//    }
+
+    /*
+    Inflation is calculated using the following
+    1. calculate tally of votes based on "inflationDest" set on each account
+    2. take the top accounts (by vote) that get at least .05% of the vote
+    3. If no accounts are over this threshold then the extra goes back to the
+       inflation pool
+    */
+
+//    int64_t totalVotes = lh.totalCoins;
+//    int64_t minBalance =
+//        bigDivide(totalVotes, INFLATION_WIN_MIN_PERCENT, TRILLION, ROUND_DOWN);
+//
+//    auto winners = ltx.queryInflationWinners(INFLATION_NUM_WINNERS, minBalance);
+
+    auto inflationAmount = 0;
+    auto amountToDole =  inflationAmount+ lh.feePool;
+
+    lh.feePool = 0;
+    lh.inflationSeq++;
+
+    // now credit each account
+    innerResult().code(INFLATION_SUCCESS);
+    auto& payouts = innerResult().payouts();
+
+    Hash seed = sha256("Kinesis KAG Test Yield feepool");
+    SecretKey feeKey = SecretKey::fromSeed(seed);
+    AccountID feeDestination = feeKey.getPublicKey();
+    int64 toDoleThisWinner = amountToDole;
+    int64 leftAfterDole = amountToDole;
+    auto winner = stellar::loadAccount(ltx,feeDestination);
+    leftAfterDole -= toDoleThisWinner;
+    addBalance(header, winner, toDoleThisWinner);
+    payouts.emplace_back(feeDestination, toDoleThisWinner);
+
+   
+
+//    for (auto const& w : winners)
+//    {
+//        int64_t toDoleThisWinner =
+//            bigDivide(amountToDole, w.votes, totalVotes, ROUND_DOWN);
+//        if (toDoleThisWinner == 0)
+//            continue;
+//
+//        if (lh.ledgerVersion >= 10)
+//        {
+//            auto winner = stellar::loadAccountWithoutRecord(ltx, w.accountID);
+//            if (winner)
+//            {
+//                toDoleThisWinner = std::min(getMaxAmountReceive(header, winner),
+//                                            toDoleThisWinner);
+//                if (toDoleThisWinner == 0)
+//                    continue;
+//            }
+//        }
+//
+//        auto winner = stellar::loadAccount(ltx, w.accountID);
+//        if (winner)
+//        {
+//            leftAfterDole -= toDoleThisWinner;
+//            if (lh.ledgerVersion <= 7)
+//            {
+//                lh.totalCoins += toDoleThisWinner;
+//            }
+//            if (!addBalance(header, winner, toDoleThisWinner))
+//            {
+//                throw std::runtime_error(
+//                    "inflation overflowed destination balance");
+//            }
+//            payouts.emplace_back(w.accountID, toDoleThisWinner);
+//        }
+//    }
+
+    // put back in fee pool as unclaimed funds
+    lh.feePool += leftAfterDole;
+    if (lh.ledgerVersion > 7)
+    {
+        lh.totalCoins += inflationAmount;
+    }
+
+    return true;
+}
+
+#else
+// original function implementation
 
 bool
 InflationOpFrame::doApply(AbstractLedgerTxn& ltx)
@@ -115,6 +219,8 @@ InflationOpFrame::doApply(AbstractLedgerTxn& ltx)
 
     return true;
 }
+
+#endif
 
 bool
 InflationOpFrame::doCheckValid(uint32_t ledgerVersion)
